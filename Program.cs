e9c714029d62c713b3f7806c533158dad6f582a4
Felipe França <contatofelipefranca12@gmail.com>
2020-10-07 20:04:25 -0300

    Adiciona a conclusão do exercício UltimaParcela

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UltimaParcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Última Parcela");
+            DateTime dataAtual = DateTime.Now;
+            Console.Write("Qual o valor total do financiamento................:");
+            double valorFinanciamento = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Quantidade de parcelas (sem juros).................:");
+            int quantidadeParcela = Convert.ToInt32(Console.ReadLine());
+            double valorParcela = valorFinanciamento / quantidadeParcela;
+            DateTime ultimaParcela = dataAtual.AddMonths(quantidadeParcela);
+            Console.Write($"Última parcela será em {ultimaParcela.ToShortDateString()}, no valor de {valorParcela}.");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # UltimaParcela
-[console: UltimaParcela] Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. Exiba a data e o valor da última parcela.
+*[console: UltimaParcela] Receba o valor total a ser pago e a quantidade de parcelas mensais de um financiamento. Exiba a data e o valor da última parcela.*
+
+`Exemplo(vamos considerar a data atual 07/10/2020):`
+
+```
+--- Última Parcela
+Qual o valor total do financiamento................:985,12
+Quantidade de parcelas (sem juros).................:8
+Última parcela será em 07/06/2021, no valor de 123,14.
+```
